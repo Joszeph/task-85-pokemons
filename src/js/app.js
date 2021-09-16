6ee152f5520bb7460ec83745dac93e2269069024
martin pandarski <martin.pandarski@boom.dev>
2021-09-16 15:06:23 +0300

    feat: add boilerplate for task-85

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,5 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  const ul = document.querySelector("ul");
 });
