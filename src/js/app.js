5b90f1786f9300428c8b52c2a45d18ab6e9bfd43
Yosif Georgiev <iodesign@live.com>
2023-01-10 13:05:46 +0200

    NEW: fetch pokemons

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,20 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const ul = document.querySelector('ul');
+    data.results.forEach(pokemon => {
+        const li = document.createElement('li');
+        li.innerHTML = pokemon.name;
+        ul.appendChild(li);
+    });
+  })
+  .catch(error => console.error(error));
+
+
+
 });
